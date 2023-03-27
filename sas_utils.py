14f069ab5aeffb19a329eeddee44381c5c3e53f5
Eamonn Work <eamonn.shirey@gmail.com>
2023-03-26 21:22:18 -0400

    Updated some stuff

--- sas_utils.py
+++ sas_utils.py
@@ -36,3 +36,37 @@ def shuffle_tasks(df):
                     # print('Failed because:', n, task_list[n][k])
 
     return (task_list)
+
+
+
+### OLD  CODE
+
+import pandas as pd
+import smtplib
+import ssl
+import pickle
+import sas_utils
+
+with open('test.pkl', 'rb') as file:
+    vars = pickle.load(file)
+port = vars[0]
+password = vars[1]
+
+
+smtp_server = "smtp.gmail.com"
+sender_email = "kylesgonnahatethis@gmail.com"  # Enter your address
+receiver_email = "eamonn.shirey@gmail.com"  # Enter receiver address
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+
+
+
+target = 'https://docs.google.com/spreadsheets/d/1FTQo7Int8YRHdBe6-QRKzJ0dr1uU3SZLaHRypyWudiY/edit#gid=1372196122'
+df = sas_utils.read_proposals(target)
--- secret_agent_santa.py
+++ secret_agent_santa.py
@@ -1,29 +1,61 @@
-import pandas as pd
+
+
+import random
 import smtplib
-import ssl
-import pickle
-import sas_utils
-
-with open('test.pkl', 'rb') as file:
-    vars = pickle.load(file)
-port = vars[0]
-password = vars[1]
-
-
-smtp_server = "smtp.gmail.com"
-sender_email = "kylesgonnahatethis@gmail.com"  # Enter your address
-receiver_email = "eamonn.shirey@gmail.com"  # Enter receiver address
-message = """\
-Subject: Hi there
-
-This message is sent from Python."""
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
 
 
 
-target = 'https://docs.google.com/spreadsheets/d/1FTQo7Int8YRHdBe6-QRKzJ0dr1uU3SZLaHRypyWudiY/edit#gid=1372196122'
-df = sas_utils.read_proposals(target)
+# Ask each person to submit a task
+tasks = {}
+for name, email in family.items():
+    task = input(f'Hi {name}, please enter a task for the person without a partner: ')
+    tasks[name] = task
+
+# Shuffle the list of names to ensure random pairings
+names = list(family.keys())
+random.shuffle(names)
+
+# Remove one person from the list of names
+no_partner = random.choice(names)
+names.remove(no_partner)
+
+# Create a dictionary to hold the pairings
+pairings = {}
+
+# Loop through the shuffled list of names and assign each person to the next person in the list (wrapping around to the beginning if necessary),
+# making sure they are not paired with their spouse
+for i in range(len(names)):
+    giver = names[i]
+    receiver = names[(i + 1) % len(names)]
+    while family.get(giver) == receiver or family.get(receiver) == giver:
+        # If the giver and receiver are spouses, reassign the receiver
+        receiver = names[(names.index(receiver) + 1) % len(names)]
+    pairings[giver] = receiver
+
+# If there is no partner for one person, assign them the task submitted by each person
+if no_partner:
+    task_list = '\n'.join(tasks.values())
+    pairings[no_partner] = task_list
+
+# Send an email to each person with their assigned gift recipient or task list
+smtp_server = 'smtp.example.com'  # Replace with your SMTP server
+smtp_port = 587  # Replace with your SMTP port
+smtp_username = 'your_username'  # Replace with your SMTP username
+smtp_password = 'your_password'  # Replace with your SMTP password
+
+for giver, recipient in pairings.items():
+    sender_email = family[giver]
+    if type(recipient) == str:
+        # If the recipient is a task list, format the email message accordingly
+        message = f'Subject: Secret Santa\n\nHi {giver},\n\nUnfortunately, we were unable to pair you up with anyone for this year\'s Secret Santa gift exchange. Instead, you have been assigned the following tasks:\n\n{recipient}\n\nBest regards,\nYour Secret Santa Bot'
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.sendmail(sender_email, sender_email, message)
+    else:
+        # If the recipient is a person, format the email message accordingly
+        message = f'Subject: Secret Santa\n\nHi {giver},\n\nYou have been assigned to buy a gift for {recipient}. Happy holidays!\n\nBest regards,\nYour Secret Santa Bot'
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.sendmail(sender_email, family[recipient], message)
