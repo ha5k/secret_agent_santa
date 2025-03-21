a4ea6a68c29578dd103c99a5427ab3ce6b5c35ad
Eamonn Work <eamonn.shirey@gmail.com>
2025-03-21 11:27:34 -0400

    Started building email structure... this feels real

--- draw_route_cards.py
+++ draw_route_cards.py
@@ -13,7 +13,7 @@ from random import shuffle
 
 # If it has a new entry, assign that person a username and add them to the family
 for k in range(len(route_requests)):
-    name = route_requests['Who Are You?'][k] + '_' + route_requests['Timestamp'][k]
+    name = route_requests['Who Are You?'][k] + '_Bonus_' + route_requests['Timestamp'][k]
     email = route_requests['Email']
     if name not in family:
         family[name] = sas_utils.person(name, email, '', True)
@@ -22,15 +22,44 @@ for k in range(len(route_requests)):
 unused_tasks = sas_utils.get_unused_tasks(family)
 shuffle(unused_tasks)
 
+send_routes_to = []
 for nr in family:
     if len(family[nr].selections) == 0:
+        send_routes_to.append(nr)
         for k in min(3, len(unused_tasks)):
             family[nr].selections.append(unused_tasks.pop(0))
             family[nr].selections[-1].selected = True
 
  # Email options and remove options from backup list
 
-## Read in form for selecting new route cards
+with smtplib.SMTP('smtp.gmail.com', facilitator['port']) as server:
+    server.starttls()
+    server.login(facilitator['email'], facilitator['pwd'])
+
+    for nr in send_routes_to:
+        subject = 'Subject: {}\n\n'.format('Your Secret Agent Santa Bonus Route Cards')
+        message = '\n'.join([
+            subject,
+            f"Hey there, {nr.split('_')[0]}\n",
+            "You're a crazy person and asked for more tasks. It's too late to take it back now.\n",
+            "The additional tasks you can choose from are:\n",
+
+            f"\nTask A: {family[member].selections[0].title}",
+            f"{family[member].selections[0].details}"
+            f"\nTask B: {family[member].selections[1].title}",
+            f"{family[member].selections[1].details}"
+            f"\nTask C: {family[member].selections[2]}\n",
+            f"{family[member].selections[2].details}"
+
+            f"When selecting your task, the form will ask for a username. Your username for this round of drawing route cards is:{nr}",
+
+            "\nPlease make your selection here:",
+            forms['select_routes'][0] + '\n',
+
+        ## Read in form for selecting new route cards
+route_selections = sas_utils.read_form(forms['draw_routes'][1])
+
+
 
  # If it has an entry with assigned username and selection, confirm selection
 
