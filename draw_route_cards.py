617b699ff9fc3e1b66d9b711930b0f3e1f387c07
Eamonn Work <eamonn.shirey@gmail.com>
2025-03-21 09:05:19 -0400

    Added shell for drawing route cards

---
+++ draw_route_cards.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+import smtplib
+import pickle
+import numpy as np
+import sas_utils
+from time import time
+from random import shuffle
+
+## Read In Form for Drawing Route Cards
+
+ # If it has a new entry, assign that person a username and tasks to choose
+
+ # Email options and remove options from backup list
+
+## Read in form for selecting new route cards
+
+ # If it has an entry with assigned username and selection, confirm selection
+
+ # Email confirmation to the chooser
+
+ # Email the confirmed task to the secret agent
